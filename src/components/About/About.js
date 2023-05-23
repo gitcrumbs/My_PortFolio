b8fa978c2a8a1a1219dc7ad0e06b7c9a479218cf
gitcrumbs <ashwanisingh034@gmail.com>
2023-05-23 23:09:37 +0530

    Updated About Me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -26,16 +26,10 @@ const About = () => {
           </Flip>
           <Fade right>
             <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English. Many desktop
-              publishing packages and web page editors now use Lorem Ipsum as
-              their default model text, and a search for 'lorem ipsum' will
-              uncover many web sites still in their infancy. Various versions
-              have evolved over the years, sometimes by accident, sometimes on
-              purpose (injected humour and the like).
+            I aim to pursue a challenging career and be part of a progressive
+            organization that gives me scope to enhance my knowledge, skills and
+            help me reach the pinnacle in the field present, with sheer determination
+            and dedication.
             </p>
           </Fade>
         </div>
